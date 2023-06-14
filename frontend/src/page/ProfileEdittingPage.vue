6a729a70356a79c8f6d3686481a4618b5e482b91
嘎啦嘎拉 <sam880629@gmail.con>
2023-06-14 08:39:07 +0800

    會員更改頁

--- frontend/src/page/ProfileEdittingPage.vue
+++ frontend/src/page/ProfileEdittingPage.vue
@@ -1,11 +1,86 @@
 <template>
     <div>
-        
+        <h2 class="title">編輯個人資料</h2>
+        <div class="changeAvatar">
+            <TheAvatar :width="48" :height="48" />
+            <TheButton>修改頭像</TheButton>
+            <input type="file" class="inputFile">
+        </div>
     </div>
+    <form class="profileForm">
+        <label for="username">用戶名稱:</label>
+        <input type="text" />
+        <label for="name">暱稱:</label>
+        <input type="text">
+        <label for="intro">簡介:</label>
+        <textarea rows="12"></textarea>
+        <label for="mobilePhone">手機號碼:</label>
+        <input type="text">
+        <label>性別:</label>
+        <div class="genderRadios">
+            <input type="radio" name="gender" id="M" value="M" />
+            男
+            <input type="radio" name="gender" id="F" value="F" />
+            女
+        </div>
+        <label for="website">連結:</label>
+        <input type="text" />
+        <div class="actions">
+            <TheButton type="reset" reverse>取消</TheButton>
+            <TheButton type="submit" reverse>f確定</TheButton>
+        </div>
+    </form>
 </template>
 
-<script>
+<script setup>
+import TheAvatar from '../components/TheAvatar.vue';
+
 </script>
 
-<style>
+<style scoped>
+.title {
+  margin-bottom: 42px;
+  font-size: 24px;
+  font-weight: 600;
+}
+
+.changeAvatar {
+  display: flex;
+  align-items: center;
+  position: relative;
+}
+
+.changeAvatar .button {
+  margin-left: 26px;
+}
+
+.inputFile {
+  opacity: 0;
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  bottom: 0;
+}
+
+.profileForm {
+  display: grid;
+  grid-template-columns: max-content 1fr;
+  row-gap: 32px;
+  column-gap: 10px;
+  margin-top: 38px;
+}
+
+.profileForm > label {
+  grid-column: 1 / 2;
+  justify-self: end;
+  position: relative;
+  top: 6px;
+}
+
+.profileForm .actions {
+  grid-column: 1 / 3;
+  justify-self: end;
+  display: flex;
+  gap: 16px;
+}
 </style>
